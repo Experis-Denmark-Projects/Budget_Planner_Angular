4dccdaf5e9e91d5c4ca5f770777563ab7098cd3e
Alexander Jonstrup <Alexander.Jonstrup@outlook.com>
2023-11-07 11:16:50 +0100

    Co-authored-by: Sigurd Andersen <holyfailer@users.noreply.github.com>
    Updated AuthService.

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,43 +1,60 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment.development';
-import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http'
 import { AuthService as Auth0AuthService } from '@auth0/auth0-angular';
 import { User } from '../models/user.model';
-import { switchMap } from 'rxjs/operators'
+import { catchError, filter, switchMap, takeWhile } from 'rxjs/operators'
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   accessToken:string = ''
+  idToken:string = ''
   user:User = {};
   private apiUrl = environment.apiUrl;
 
   constructor(private auth0:Auth0AuthService, private http: HttpClient) { 
 
+    let headers = {}
+
     this.auth0.getAccessTokenSilently().pipe(
       // Get Access Token
       switchMap((token:any) => {
-        console.log(`Access Token:${token}`)
         this.accessToken = token
-        const headers = new HttpHeaders({
+        headers = new HttpHeaders({
           'Authorization': `Bearer ${this.accessToken}`,
           'Content-Type': `application/json`,
         })
-        return this.http.post(`${this.apiUrl}/private/user`, {
+        return token
+      }),
+      switchMap(() => {
+        return this.http.get(`${this.apiUrl}/private/user`, {
           headers: headers,
           withCredentials: true,
           observe: 'response',
           responseType: 'json'
-        })
+        }).pipe(
+          catchError(error => of(new HttpResponse({status: 400})))
+        )
       }),
+      // If user does not exists run next switchMap otherwise skip
+      filter((response) => response.status === 400),
+      switchMap(() => {
+        return this.http.post(`${this.apiUrl}/private/user`, {}, {
+          headers: headers,
+          withCredentials: true,
+          observe: 'response',
+          responseType: 'json'
+        }).pipe(
+          catchError(error => of(new HttpResponse({status: 400})))
+        )
+      }), 
+      takeWhile((response) => response.status !== 400),
+      // Get user if response status is not 400.
       switchMap((user:any) => {
-        console.log(`User:${user}`)
         this.user = {...user, categories: []}
-        const headers = new HttpHeaders({
-          'Authorization': `Bearer ${this.accessToken}`,
-          'Content-Type': `application/json`,
-        })
         return this.http.get(`${this.apiUrl}/private/user/category`, {
           headers: headers,
           withCredentials: true,
@@ -46,16 +63,15 @@ export class AuthService {
         })
       }),
       switchMap((categories:any) => {
-        console.log(`Categories:${categories}`)
         this.user.categories = categories
         return categories
       })
-    ).subscribe({
-      next: () => {
-        console.log('Called!')
-      }
+    ).subscribe(() => {
+      console.log(`Username: ${this.user.username}`)
+      console.log(`Email: ${this.user.email}`)
+      console.log(`Total Budget: ${this.user.totalBudget}`)
+      console.log(`Categories: ${this.user.categories}`)
     })
-
   }
 
   login() {
@@ -71,10 +87,6 @@ export class AuthService {
   }
 
   logout(){
-
-  }
-
-  signup(){
-
+    this.auth0.logout();
   }
 }
